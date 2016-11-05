40f8eca7bbfbb9643ee0473774fb1a51ca3afa14
洋佑 堀川 <uplifting0760@gmail.com>
2016-11-05 15:50:27 +0900

    add time

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,13 @@ package main
 
 import( 
 	"fmt"
-
+	"time"
 	"github.com/CASIXx1/stringutill"
 )
 
 func main() {
 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
 }
